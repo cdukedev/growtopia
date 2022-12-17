ef26c04d4f9d7149bd9cd8da7200d267523ec3b0
macapedi <macapedi90@gmail.com>
2022-12-17 14:43:14 -0500

    home page and addplant page added

---
+++ src/pages/AddPlantPage/AddPlantPage.js
@@ -0,0 +1,13 @@
+import './AddPlantPage.css';
+
+
+function AddPlantPage() {
+  return (
+    <div className="App">
+      <h1>This is Add Plant Page</h1>
+      
+    </div>
+  );
+}
+
+export default AddPlantPage;
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+
+import './HomePage.css';
+
+
+function HomePage() {
+  return (
+    <div className="App">
+      <h1>This is HOME PAGE</h1>
+      
+    </div>
+  );
+}
+
+export default HomePage;
