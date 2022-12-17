00271cbb0f3e3fe7b6d624c59da6c7e3758b4a1e
macapedi <macapedi90@gmail.com>
2022-12-17 14:38:04 -0500

    color added to header and original react logo erased

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,13 @@
 import "./Header.scss";
 import { Link } from "react-router-dom";
-import logo from "../../assets/logo.png";
+
 
 function Header() {
   return (
     <div className="header">
-      <Link to="/home">
-        <img src={logo} alt='navbar logo' aria-label="the logo is a picture of a plant" className="header__logo"></img>
-      </Link>
+      {/* <Link to="/home">
+        {/* <img src={logo} alt='navbar logo' aria-label="the logo is a picture of a plant" className="header__logo"></img> */}
+      {/* </Link> */} 
         <h1 className="header__title">Growtopia</h1>
     </div>
   );
