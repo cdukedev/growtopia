2feed48e5bdd8ce253ca85e0d3ddcfea71bf492d
corey duke <cdukedev@gmail.com>
2022-12-18 00:17:11 -0500

    adjusted breaking changes for the team to begin their work

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,19 +4,18 @@ import DraggableDiv from "../../components/DraggableDiv/DraggableDiv";
 import { data } from "../../data/user";
 
 function HomePage() {
-  const plants = data.get("plants");
-  console.log(plants);
-  const utilities = data.get("utilities");
-  
+  // const plants = data.get("plants");
+  // console.log(plants);
+  // const utilities = data.get("utilities");
 
-  const draggableItems = data.map((item) => (
-    <DraggableDiv key={item.id} name={item.name} />
-  ));
+  // const draggableItems = data.map((item) => (
+  //   <DraggableDiv key={item.id} name={item.name} />
+  // ));
   return (
     <div className="App">
       <h1>This is HOME PAGE</h1>
       <CircleButton />
-      <div className="draggable-container">{draggableItems}</div>
+      {/* <div className="draggable-container">{draggableItems}</div> */}
     </div>
   );
 }
