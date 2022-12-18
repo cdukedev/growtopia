2b024f51ca0e1b50d52d7a5ea59a598b74f8d799
macapedi <macapedi90@gmail.com>
2022-12-18 02:30:11 -0500

    button added to single plant page

--- src/pages/SinglePlantPage/SinglePlantPage.js
+++ src/pages/SinglePlantPage/SinglePlantPage.js
@@ -12,6 +12,7 @@ function SinglePlantPage() {
         </div>
         <img src={tomaticos} className="single-plant-image"></img>
         <p>Tomato plants are members of the nightshade family (Solanaceae) and are native to South America. They are typically grown in warm, sunny climates and are sensitive to frost. Tomato plants can grow to be quite large, reaching heights of up to 10 feet or more. They have a branching, vine-like growth habit and typically produce clusters of small, yellow flowers that give way to the tomato fruit.</p>
+        <button className="card-button">+</button>
         
 
       </div>
