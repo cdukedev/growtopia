f39647440f8a216f2cbfbf46bd8b2ab5e6e79351
corey duke <cdukedev@gmail.com>
2022-12-18 09:51:22 -0500

    Merge branch 'main' into duke

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,16 +1,19 @@
 import "./HomePage.scss";
 import CircleButton from "../../components/CircleButton/CircleButton";
 import React, { useState, useEffect } from "react";
+
 import CPButton from "../../components/CPButton/CPButton";
 import DraggableDiv from "../../components/DraggableDiv/DraggableDiv";
 import { data } from "../../data/user";
-
 function HomePage() {
   return (
     <div className="App">
       <CPButton />
       <DraggableDiv />
       <CircleButton />
+      <img 
+        className="background"
+        src="https://i.imgur.com/ftThhhs.png" alt="" />
     </div>
   );
 }
