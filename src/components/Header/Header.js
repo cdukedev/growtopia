d8bbf6b0d0f8395d8b043ecbbe2c8d9e96e0280a
macapedi <macapedi90@gmail.com>
2022-12-18 09:10:07 -0500

    icons added to assets folder, still no show in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,21 @@
 import "./Header.scss";
 import { Link } from "react-router-dom";
+import avatar from "../../assets/avatar.png";
+import bell from "../../assets/bell.png";
 
 function Header() {
   return (
-    <div className="header">
-      <Link to="/home">
-        <h2 className="header__title">Growtopia</h2>
-      </Link>
-    </div>
+    <>
+      <div className="header">
+        <Link to="/home" >
+          <h2 className="header__title">Growtopia</h2>
+        </Link>
+      </div>
+      <div className="header-icons">
+        <image src={bell} className="icon"></image>
+        <image src={avatar} className="avatar"></image>
+      </div>
+    </>
   );
 }
 
