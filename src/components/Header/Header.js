3daaa08119a4d7e69836316a4d68fc0747bd5e3f
macapedi <macapedi90@gmail.com>
2022-12-18 06:46:34 -0500

    header Logo linked and aligned left

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,12 @@
 import "./Header.scss";
 import { Link } from "react-router-dom";
 
-
 function Header() {
   return (
     <div className="header">
-      {/* <Link to="/home">
-        {/* <img src={logo} alt='navbar logo' aria-label="the logo is a picture of a plant" className="header__logo"></img> */}
-      {/* </Link> */} 
-        <h1 className="header__title">Growtopia</h1>
+      <Link to="/home">
+        <h2 className="header__title">Growtopia</h2>
+      </Link>
     </div>
   );
 }
