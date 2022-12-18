e65aab3556fa9d94d699ec5102272372c19da623
corey duke <cdukedev@gmail.com>
2022-12-18 00:15:03 -0500

    ADDED BUTTONs, homepage and draggable div components

---
+++ src/data/user.js
@@ -0,0 +1,24 @@
+const data = new Map();
+
+data.set("plants", [
+  { name: "Tomato" },
+  { name: "Mango" },
+  { name: "Mango" },
+  { name: "Tomato" },
+  { name: "Tomato" },
+  { name: "Strawberry" },
+  { name: "Strawberry" },
+  { name: "Strawberry" },
+  { name: "Papaya" },
+]);
+
+data.set("cp", "286");
+
+data.set("utilities", [
+  { garden: "box1", x: "1", y: "1" },
+  { garden: "box2", x: "15", y: "15" },
+  { compost: "1", x: "30", y: "30" },
+  { water: "1", x: "45", y: "45" },
+  { bees: "1", x: "60", y: "60" },
+]);
+export { data };
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,12 +1,22 @@
-
-import './HomePage.scss';
-
+import "./HomePage.scss";
+import CircleButton from "../../components/CircleButton/CircleButton";
+import DraggableDiv from "../../components/DraggableDiv/DraggableDiv";
+import { data } from "../../data/user";
 
 function HomePage() {
+  const plants = data.get("plants");
+  console.log(plants);
+  const utilities = data.get("utilities");
+  
+
+  const draggableItems = data.map((item) => (
+    <DraggableDiv key={item.id} name={item.name} />
+  ));
   return (
     <div className="App">
       <h1>This is HOME PAGE</h1>
-      
+      <CircleButton />
+      <div className="draggable-container">{draggableItems}</div>
     </div>
   );
 }
