ca33f85c8125130e7e661a5f4a3fcdb1e6e73289
macapedi <macapedi90@gmail.com>
2022-12-18 00:54:40 -0500

    zip code div added

--- src/pages/AddPlantPage/AddPlantPage.js
+++ src/pages/AddPlantPage/AddPlantPage.js
@@ -1,11 +1,14 @@
-import './AddPlantPage.scss';
-
+import "./AddPlantPage.scss";
 
 function AddPlantPage() {
   return (
     <div className="App">
       <h1>This is Add Plant Page</h1>
-      
+      <div className="zip-container">
+        <h4>Type your zip code</h4>
+        <input className="input"></input>
+        <button className="zip-button">ENTER</button>
+      </div>
     </div>
   );
 }
